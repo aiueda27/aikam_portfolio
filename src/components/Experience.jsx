a590d56ea26161e9aaf34fe6f71041625a65d9bc
aiueda27 <aiueda27@gmail.com>
2023-11-08 18:06:11 +0800

    🩹 fix: rename and restractured section header component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,16 +1,16 @@
 import { VerticalTimeline } from 'react-vertical-timeline-component'
 
 import 'react-vertical-timeline-component/style.min.css'
-import { SectionTitle } from './common'
+import { SectionHeader } from './common'
 
 import { experiences } from '../constants'
 import { SectionWrapper } from '../hoc'
-import { ExperienceCard } from './common'
+import { ExperienceCard } from './common/card'
 
 const Experience = () => {
   return (
     <>
-      <SectionTitle
+      <SectionHeader
         subText="What I have done so far"
         headText="Work Experience"
       />
