b826dbd5a14a75fa9823e492332a7273223c2cc6
aiueda27 <aiueda27@gmail.com>
2023-11-09 10:23:31 +0800

    :sparkles: feat: add feedback section

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { styles } from '../styles'
+import { SectionWrapper } from '../hoc'
+import { testimonials } from '../constants'
+import SectionHeader from './common/SectionHeader'
+import { FeedbackCard } from './common/card'
 
 const Feedbacks = () => {
   return (
-    <div>Feedbacks</div>
+    <div className="mt-12 bg-black-100 rounded-[20px]">
+      <div
+        className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}
+      >
+        <SectionHeader subText="What others say" headText="Testimonails" />
+      </div>
+      <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-col gap-7`}>
+        {testimonials.map((testimonial, index) => (
+          <FeedbackCard key={testimonial.name} index={index} {...testimonial} />
+        ))}
+      </div>
+    </div>
   )
 }
 
-export default Feedbacks
+export default SectionWrapper(Feedbacks, '')
